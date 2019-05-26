241bf6f5801c7970b459e6ea5210e70ac41eae55
kevin lang <langkevina@gmail.com>
2019-05-26 15:45:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,33 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  cnt = 0
+  array.each do |item|
+    cnt += 1
+    puts("#{cnt}.  " + item)
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  # .capitalize
+  i = 0
+ block_return_values = []
+ while i < array.length
+  #  block_return_values << array[i]
+   block_return_values[i] = array[i].capitalize
+   i = i + 1
+ end
+  return block_return_values
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+   array.each do |item|
+     if item.length > 4
+       return true
+     end
+   end
+  return false
 end
 
 def find_the_cheese# code an argument here
