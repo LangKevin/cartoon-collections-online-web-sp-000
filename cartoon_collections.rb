bf39ef7c36b3b4de1accde350db2a630c8b75e46
kevin lang <langkevina@gmail.com>
2019-05-26 15:52:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,7 +30,14 @@ def long_planeteer_calls(array)# code an argument here
   return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+# famous_cats.index("Puss in Boots")
+  array.each do |item|
+    if cheese_types.index(item) != nil
+      return cheese_types.index(item)
+    end   
+  end
+  return nil
 end
